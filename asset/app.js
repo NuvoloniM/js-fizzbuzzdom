3ca75e8468fe4912aa1ed345aef7cf793d86e547
NuvoloniM <nuvoloni.m@gmail.com>
2022-03-11 16:31:43 +0100

    Ciclo che scrive in html classe e testo

--- asset/app.js
+++ asset/app.js
@@ -16,9 +16,38 @@ Applica uno stile differente a seconda del valore dell'indice per i multipli di 
 let box = document.getElementById('card_container')
 
 //ciclo per scrivere da 1 a 100 nel div
+// for (let i = 1; i <= 100; i++) {
+//     box.innerHTML += `
+//     <div class="card">
+//         <div class="number">${i}</div>
+//     </div`;
+// }
+
+/* 1 Milestone 
+Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+*/
 for (let i = 1; i <= 100; i++) {
-    box.innerHTML += `
-    <div class="card">
-        <div class="number">${i}</div>
-    </div`;
+    if (i % 3 == 0 && i % 5 == 0) {
+        box.innerHTML += `
+        <div class="card">
+            <div class="number fizzbuzz"> FizzBuzz </div>
+        </div>`
+    } else if (i % 3 == 0){
+        box.innerHTML += `
+        <div class="card">
+            <div class="number fizz"> Fizz </div>
+        </div>`
+    } else if (i % 5 == 0){
+        box.innerHTML += `
+        <div class="card">
+            <div class="number buzz">Buzz</div>
+        </div>`
+    } else {
+        box.innerHTML += `
+        <div class="card">
+            <div class="number normal"> ${i} </div>
+        </div>`
+    }
+    
 }
